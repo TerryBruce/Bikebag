0ff637158f90888ed36a168930748cbd394cfc4a
Terry Bruce <jtindiepodcast@gmail.com>
2017-09-16 09:59:36 -0400

    Update baglight-rev1.ino
    added comments to make code more understandable

--- baglight-rev1.ino
+++ baglight-rev1.ino
@@ -1,7 +1,9 @@
 
 #include <Adafruit_NeoPixel.h>
 #define PIN 4
+// pin 9 for first switch wired between 5v and pin9 with 10k resistor between pin and ground
 #define inPin 9
+//pin 10 for second switch wired between 5v and pin9 with 10k resistor between pin and ground
 #define inPin1 10
 
 // 3 8led neopixel sticks wired to pin 6 for baglight or helmet light
@@ -16,24 +18,26 @@ int val1n;
 int lightstate = 0;
 
 void setup() {
-  pinMode(inPin, INPUT);
-  pinMode(inPin1, INPUT);
-  val = digitalRead(inPin);
+  pinMode(inPin, INPUT); // first(mode) switch setup
+  pinMode(inPin1, INPUT); // second(bright) switch setup
+  val = digitalRead(inPin); // read mode switch to set base value
   pixel.begin();
   pixel.setBrightness(85); // 1/3 brightness
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-valn = digitalRead(inPin);
-val1n = digitalRead(inPin1);
+valn = digitalRead(inPin); // read mode switch to see if it has changed
+val1n = digitalRead(inPin1); // read bright switch
 delay(50);
+//set brightness to match state of bright switch
 if (val1n == 0){
 pixel.setBrightness(85);  
 }
 else {
 pixel.setBrightness(255);  
 }
+// if mode switch has changed go to next light state and change animation of lights.  
 if (valn != val){
 val = valn;
 lightstate = lightstate +1;
